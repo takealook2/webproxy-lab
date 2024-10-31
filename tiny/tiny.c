e2024acd5e8441e6a0807943185b829f8e54901f
root <root@seoha.>
2024-10-31 20:50:39 +0900

    tiny.c clenterror() 추가

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -17,6 +17,62 @@ void serve_dynamic(int fd, char *filename, char *cgiargs);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
                  char *longmsg);
 
+void doit(int fd){
+  int is_static;
+  struct stat sbuf;
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+  char filename[MAXLINE], cgiargs[MAXLINE];
+  rio_t rio;
+
+  // read request line and headers
+  Rio_readinitb(&rio, fd);
+  Rio_readlineb(&rio, buf, MAXLINE);
+  printf("Request headers: \n");
+  printf("%s, buf");
+  sscanf(buf, "%s %s %s", method, uri, version);
+  if (strcasecmp(method, "GET")) {
+    clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
+    return;
+  }
+  read_requesthdrs(&rio);
+
+  // 404
+  is_static = parse_uri(uri, filename, cgiargs);
+  if(stat(filename, &sbuf) < 0) {
+    clienterror(fd, filename, "404", "Not Found", "Tiny couldn't find this file");
+    return;
+  }
+
+  if(is_static){ //serve static content
+    if(!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)){
+      clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
+      return;
+    }
+    serve_static(fd, filename, sbuf.st_size);
+  }
+  else { //serve dynamic content
+    if(!(S_IRSEG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)){
+      clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program");
+      return;
+    }
+    serve_dynamic(fd, filename, cgiargs);
+  }
+}
+
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg){
+  char buf[MAXSIZE], body[MAXBUF];
+
+  // build the http response body
+  sprintf(body, "<html><title>Tiny Error</title>")
+  sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+  // 여기서부터
+  sprintf(body, "<html><title>Tiny Error</title>")
+  sprintf(body, "<html><title>Tiny Error</title>")
+  sprintf(body, "<html><title>Tiny Error</title>")
+  sprintf(body, "<html><title>Tiny Error</title>")
+}
+
+
 int main(int argc, char **argv) {
   int listenfd, connfd;
   char hostname[MAXLINE], port[MAXLINE];
